7f77453c67de8d882baf28a7d8f6b4520b132926
Sebastián Carabali <secarabali@gmail.com>
2024-09-17 11:32:35 -0500

    Add command finished

--- src/main/java/com/zebsoft/Main.java
+++ src/main/java/com/zebsoft/Main.java
@@ -1,21 +1,28 @@
 package com.zebsoft;
 
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.Map;
 
+import com.zebsoft.command.AddExpenseCommand;
+import com.zebsoft.command.CommandRegistry;
 import com.zebsoft.db.DBManager;
-import com.zebsoft.model.Expense;
 import com.zebsoft.parser.ArgParser;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     if (args.length < 1) {
       throw new IllegalArgumentException("usage: expense-tracker <command> [options]");
     }
+    DBManager dbManager = new DBManager("expense-tracker.json");
+    CommandRegistry commandRegistry = new CommandRegistry();
+    commandRegistry.registerCommand("add", new AddExpenseCommand());
+
     String command = args[0];
     Map<String, String> options = ArgParser.parse(args);
-    DBManager dbManager = new DBManager<>("expense-tracker.json", Expense.class);
+    
+    String response = commandRegistry.executeCommand(command, options, dbManager);
+    System.out.println(response);
   }
 
 }
--- src/main/java/com/zebsoft/db/DBManager.java
+++ src/main/java/com/zebsoft/db/DBManager.java
@@ -8,21 +8,20 @@ import java.util.Optional;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zebsoft.model.Expense;
 
-public class DBManager<T> {
+public class DBManager {
     private final File dbFile;
     private final ObjectMapper objectMapper;
-    private final Class<T> clazz;
 
-    public DBManager(String filename, Class<T> clazz) {
+    public DBManager(String filename) {
         this.dbFile = new File(filename);
         this.objectMapper = new ObjectMapper();
-        this.clazz = clazz;
         // Initialize the file if it doesn't exist
         if (!dbFile.exists()) {
             try {
                 dbFile.createNewFile();
-                objectMapper.writeValue(dbFile, new ArrayList<T>());
+                objectMapper.writeValue(dbFile, new ArrayList<Expense>());
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -30,37 +29,42 @@ public class DBManager<T> {
     }
 
     // Create or Update an object
-    public void save(T obj) throws IOException {
-        List<T> objects = readAll();
+    public void save(Expense obj) throws IOException {
+        List<Expense> objects = readAll();
         objects.add(obj);
         objectMapper.writeValue(dbFile, objects);
     }
 
     // Read all objects from file
-    public List<T> readAll() throws IOException {
+    public List<Expense> readAll() throws IOException {
         if (!dbFile.exists() || dbFile.length() == 0) {
             return new ArrayList<>();
         }
-        return objectMapper.readValue(dbFile, new TypeReference<List<T>>() {
+        return objectMapper.readValue(dbFile, new TypeReference<List<Expense>>() {
         });
     }
 
     // Read an object by a custom matcher (e.g., id match)
-    public Optional<T> findBy(Matcher<T> matcher) throws IOException {
+    public Optional<Expense> findBy(Matcher matcher) throws IOException {
         return readAll().stream()
                 .filter(matcher::matches)
                 .findFirst();
     }
 
+    // Read all object that matches a custom matcher
+    public List<Expense> findAllBy(Matcher matcher) throws IOException {
+        return readAll().stream().filter(matcher::matches).toList();
+    }
+
     // Delete an object by a custom matcher (e.g., id match)
-    public void delete(Matcher<T> matcher) throws IOException {
-        List<T> objects = readAll();
+    public void delete(Matcher matcher) throws IOException {
+        List<Expense> objects = readAll();
         objects.removeIf(matcher::matches);
         objectMapper.writeValue(dbFile, objects);
     }
 
     // Functional interface for matching objects
-    public interface Matcher<T> {
-        boolean matches(T obj);
+    public interface Matcher {
+        boolean matches(Expense obj);
     }
 }
